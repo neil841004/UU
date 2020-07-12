96626bb37e1ba49b4cc3d5b3437b283ac503080a
Halaa <neil841004@gmail.com>
2020-07-12 19:11:33 +0800

    Change Level

---
+++ Assets/Script/ChangeLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchLevel() {
+        SceneManager.LoadScene(levelName);
+    }
+
+    private void OnTriggerStay2D(Collider2D co)
+    {
+        if (co.gameObject.tag == "Player") {
+            SwitchLevel();
+        }   
+    }
+}
